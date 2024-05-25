aaca535133dbdf5218f6850ef32105da65796247
CinthSimbulan <bisimbulan1@up.edu.ph>
2024-05-26 02:09:20 +0800

    Feat: Able to add to cart and remove from cart but the transaction is not yet implemented.

--- frontend/react-project-100/src/App.js
+++ frontend/react-project-100/src/App.js
@@ -23,6 +23,18 @@ function App() {
     console.log(cart)
   };
 
+  const removeFromCart = (itemName) => {
+    // This will create a copy then reverse the array to find the index of the last added item
+    const indexToRemove = cart.slice().reverse().findIndex(item => item.name === itemName);
+    if (indexToRemove !== -1) {
+      // This will find the index of the found item when not reversed
+      const trueIndex = cart.length - 1 - indexToRemove;
+      // This will get the values of the array from the start up to before the index, and after the index up to the end
+      const updatedPushcarts = [...cart.slice(0, trueIndex), ...cart.slice(trueIndex + 1)];
+      setCart(updatedPushcarts);
+    }
+  };
+
 
   const isUserSignedIn = !!localStorage.getItem('token')
   const userType = localStorage.getItem('usertype');
@@ -35,7 +47,7 @@ function App() {
           <Route path='/login' element={<Login />} />
           <Route path='/signup' element={<SignUp />} />
           {isUserSignedIn && <Route path='/account' element={<Account sortOption={sortOption} onCartChange={addToCart} />} />}
-          <Route path='/cart' element={<Cart cart={cart} />} />
+          <Route path='/cart' element={<Cart cart={cart} onRemoveItem={removeFromCart} />} />
           {isUserSignedIn && userType === 'Merchant' && <Route path='/admin' element={<Admin />} />}
         </Routes>
       </div>
--- frontend/react-project-100/src/pages/Cart.js
+++ frontend/react-project-100/src/pages/Cart.js
@@ -1,21 +1,39 @@
 import React from 'react';
 
-function Cart({ cart }) {  // Destructure the cart prop correctly
+function Cart({ cart, onRemoveItem }) {  // Destructure the cart prop correctly
     const showCart = () => {
         console.log('Cart contents:', cart);
+        console.log('unique item names:', uniqueItemNames)
     };
+
+    let itemCounts = {};
+    cart.forEach(item => {
+        itemCounts[item.name] = (itemCounts[item.name] || 0) + 1;
+    });
+
+    const removeItem = (subject) => {
+        onRemoveItem(subject)
+    };
+
+    let uniqueItemNames = Object.keys(itemCounts);
 
     return (
         <div>
-            Shopping Cart
-            <button onClick={showCart}>Show Cart</button>
-            <ul>
-                {cart.map((item, index) => (
-                    <li key={index}>
-                        {item.name} - {item.price}
-                    </li>
+            <div className='flex justify-center font-black text-6xl'>
+                Shopping Cart
+            </div>
+            <button className="mb-4" onClick={showCart}>Show Cart</button>
+            <div>
+                <h2 id='cart'>(Total: {cart.length})</h2>
+                {uniqueItemNames.map((item, index) => (
+                    <div className="flex justify-center" key={index}>
+                        <p>{item}</p>
+                        <p>Quantity: {itemCounts[item]}</p>
+                        <button onClick={() => removeItem(item)}>Remove</button>
+                        <button>Checkout</button>
+                    </div>
                 ))}
-            </ul>
+            </div>
         </div>
     );
 }
